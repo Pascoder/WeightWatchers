9c0bec6784188fb3d0dfb1c47268122c6ec14e99
olmo <olmo@192.168.2.242>
2020-03-29 14:26:04 +0200

    Card, Game Klasse Gerüst

--- src/server/Game.java
+++ src/server/Game.java
@@ -1,5 +1,39 @@
 package server;
 
-public class Game {
+import java.util.ArrayList;
 
+public class Game {
+    
+    private ArrayList<Team> teams;
+    private CardDeck cardDeck;
+    private ArrayList<Card> cardsOnTable;
+    private CardColor trumpf;
+    private int round;
+    private int move;
+    private int gameID;
+    
+    
+    Game(){
+	
+    }
+    
+    private void startGame() {
+	
+    }
+    
+    private void generateTeams() {
+	
+    }
+
+    private void spreadCards() {
+	
+    }
+    
+    private void setPlayerOnMove() {
+	
+    }
+    
+    private void setTrumpf() {
+	
+    }
 }
--- src/server/JassModel.java
+++ src/server/JassModel.java
@@ -1,5 +1,22 @@
 package server;
 
+import java.util.ArrayList;
+
 public class JassModel {
+    
+    public static ArrayList<Card> evalHand = new ArrayList<Card>();
+    
+    
+    public JassModel(){
+	
+    }
+    
+    public static isValidMove <ArrayList> () {
+	
+	
+	return isValidMove;
+	
+    }
+    
 
 }
