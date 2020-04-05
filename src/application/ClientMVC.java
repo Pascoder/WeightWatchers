f063dd1d95fcf2463919dab5ccbf004de9570b49
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2020-04-05 20:53:41 +0200

    Created configuration method in mvc

--- src/application/ClientMVC.java
+++ src/application/ClientMVC.java
@@ -1,15 +1,26 @@
 package application;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import client.ClientController;
 import client.ClientModel;
 import client.ClientView;
+import client.Configuration_JC;
+import client.ServiceLocator_JC;
+import client.Translator_JC;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+//TODO in der initialize Methode Klasse zur Verbindungsaufbau aufruffen
 
 public class ClientMVC extends Application{
 	private ClientModel clientModel;
 	private ClientView clientView;
 	private ClientController clientController;
+	private ServiceLocator_JC sl;
 
 	public static void main(String[] args){
 		launch(args);
@@ -27,7 +38,31 @@ public class ClientMVC extends Application{
 		clientView.stop();
 	}
 	public void initialize() {
-		/* Hier wird Configuration, ServiceLocator, und Verbindung zu Server hergestellt*/
+		this.sl = sl.getServiceLocator();
+		sl.setLogger(configureLogger());
+		sl.setConfiguration(new Configuration_JC());
+		String language = sl.getConfiguration().getOption("Language");
+		sl.setTranslator(new Translator_JC(language));
+		//Hier fehlt noch Verbindung zu Server herstellen
+	}
+	private Logger configureLogger() {
+		Logger rootLogger = Logger.getLogger("");
+	    rootLogger.setLevel(Level.FINEST);
+	    // By default there is one handler: the console
+	    Handler[] defaultHandlers = Logger.getLogger("").getHandlers();
+	    defaultHandlers[0].setLevel(Level.INFO);
+	    // Add our logger
+	    Logger ourLogger = Logger.getLogger(sl.getAPP_NAME());
+	    ourLogger.setLevel(Level.FINEST);
+	    // Add a file handler, putting the rotating files in the tmp directory
+	    try {
+	    Handler logHandler = new FileHandler("%t/"+ sl.getAPP_NAME() + "_%u" + "_%g" + ".log",1000000, 9);
+	    logHandler.setLevel(Level.FINEST);
+	    ourLogger.addHandler(logHandler);
+	    } catch (Exception e) { // If we are unable to create log files
+	    throw new RuntimeException("Unable to initialize log files: "+ e.toString());
+	        }
+	    return ourLogger;
 	}
 
 }
