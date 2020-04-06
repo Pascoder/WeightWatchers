7c48d1bb673a4a808f9bf22ec0440342dd49ff01
leonxhinovci <leonxhinovci@leons-macbook-pro-2.home>
2020-04-06 20:44:26 +0200

    Ich habe einen Abstand gemacht - haha das war ein Test

--- src/client/Lobby_View.java
+++ src/client/Lobby_View.java
@@ -29,7 +29,7 @@ public class Lobby_View extends ClientView<Chatroom_Model> {
    
     
 
-    public Lobby_View(Stage stage, Chatroom_Model model ) {
+    public Lobby_View(Stage stage, Chatroom_Model model) {
 	super(stage, model);
 	sl.getLogger().info("Chatroom_View initialized");
 
@@ -43,7 +43,7 @@ public class Lobby_View extends ClientView<Chatroom_Model> {
 	messageList.setItems(messages);
 	}
     public void setChatter(ObservableList<String> chatter) {	
-	chatterList.setItems(chatter);
+	chatterList.setItems(chatter );
 	}
 
     @Override
