97328abca9e8807bd76dbeadcf35b2c5422a1fa8
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2020-04-09 10:50:35 +0200

    created Class Card

--- src/server/Card.java
+++ src/server/Card.java
@@ -1,71 +1,38 @@
 package server;
 
 public class Card implements Comparable<Card> {
-    public enum Suit { Clubs, Diamonds, Hearts, Spades;
-        @Override
-        public String toString() {
-            String suit = "";
-            switch (this) {
-            case Clubs: suit = "clubs"; break;
-            case Diamonds: suit = "diamonds"; break;
-            case Hearts: suit = "hearts"; break;
-            case Spades: suit = "spades"; break;
-            }
-            return suit;
-        }
-    };
+   
+    private final CardColor cardColor;
+    private final CardRank cardRank;
     
-    public enum Rank { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace;
-        @Override
-        public String toString() {
-            String str = "ace";  // Assume we have an ace, then cover all other cases
-            // Get ordinal value, which ranges from 0 to 12
-            int ordinal = this.ordinal();
-            if (ordinal <= 8) {
-                str = Integer.toString(ordinal+2);
-            } else if (ordinal == 9) { // Jack
-                str = "jack";
-            } else if (ordinal == 10) { // Queen
-                str = "queen";
-            } else if (ordinal == 11) { // King
-                str = "king";
-            }
-            return str;
-        }
+    public Card(CardColor cardColor, CardRank cardRank) {
+        this.cardColor = cardColor;
+        this.cardRank = cardRank;
     }
 
-    
-    private final Suit suit;
-    private final Rank rank;
-    
-    public Card(Suit suit, Rank rank) {
-        this.suit = suit;
-        this.rank = rank;
+    public CardColor getCardColor() {
+        return cardColor;
     }
 
-    public Suit getSuit() {
-        return suit;
-    }
-
-    public Rank getRank() {
-        return rank;
+    public CardRank getCardRank() {
+        return cardRank;
     }
     
     public int getOrdinal() {
-	return rank.ordinal();
+	return cardRank.ordinal();
     }
     
     @Override
     public String toString() {
-        return rank.ordinal()+"/" + rank.toString() + suit.toString();
+        return cardRank.ordinal()+"/" + cardRank.toString() + cardColor.toString();
     }
     
     @Override
 	public int compareTo(Card otherCard) {
-		if ( this.rank.ordinal() > otherCard.rank.ordinal())
+		if ( this.cardRank.ordinal() > otherCard.cardRank.ordinal())
 			return 1;
 		else
-			if (this.rank.ordinal() < otherCard.rank.ordinal())
+			if (this.cardRank.ordinal() < otherCard.cardRank.ordinal())
 				return -1;
 			else
 				return 0;
