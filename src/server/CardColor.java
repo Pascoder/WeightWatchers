9881ee2e6c3fa70b30376cfb5eb63b05600f112d
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2020-04-09 10:08:10 +0200

    Created enum CardColor, french cards are optional

--- src/server/CardColor.java
+++ src/server/CardColor.java
@@ -1,5 +1,33 @@
 package server;
 
 public enum CardColor {
+//Schweizer oder Französische (optional) Karten sind möglich
+Rose,
+Eichel,
+Schilten,
+Schellen;
+/*Spades,
+Hearts,
+Diamonds,
+Clubs;*/
+
+public String toString() {
+    String suit = "";
+    switch (this) {
+   /* case Clubs: suit = "clubs"; break;
+    case Diamonds: suit = "diamonds"; break;
+    case Hearts: suit = "hearts"; break;
+    case Spades: suit = "spades"; break;*/
+    case Rose: suit = "rose"; break;
+    case Eichel: suit = "eichel"; break;
+    case Schilten: suit = "schilten"; break;
+    case Schellen: suit = "schellen"; break;
+    }
+    return suit;
+}
 
 }
+
+
+
+
