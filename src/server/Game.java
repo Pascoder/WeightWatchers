76cff0dead87c6a5ae20d460f0e6ef78671c5a3b
olmo <olmo@192.168.2.242>
2020-04-13 12:23:25 +0200

    Update Game

--- src/server/Game.java
+++ src/server/Game.java
@@ -9,42 +9,83 @@ public class Game {
     private CardDeck cardDeck;
     private ArrayList<Card> cardsOnTable;
     private CardColor trumpf;
+    private int countTeam;
+    private int countPlayer;
     private int round;
     private int move;
     private int gameID;
     private String name;
     
-    
+    //Wird aufgerufen, wenn ein User in der Lobby ein neues Spiel erzeugt.
     Game(int gameID, String name){
+	this.teams = new ArrayList<>();
+	this.countTeam = 0;
 	this.gameID = gameID;
 	this.name = name;
+	this.move = 0;
     }
     
+    // In der Lobby wählt ein Spieler ein Game aus und wird danach automatisch einem Team hinzugefügt. 
+    //1. und 2. Spieler dem Team 1 und 3. und 4 Spieler Team2.  
+    private void generateTeams() {
+	this.
+    }
+    
+    
+    
+    
+    
+    //Sobald 2 Teams/4Spieler im Spiel vorhanden sind, muss Spiel automatisch gestartet werden.
     private void startGame() {
 	
-    }
-    
-    private void generateTeams() {
+	generateTeams();
+	//show Table
 	
     }
-
+    
+   
+    
+    //Kartenverteilen: Nach dem Mischen der Karten müssen die auf die Spieler verteilt werden.
     private void spreadCards() {
 	
     }
     
+    //Muss definieren, welcher Spieler an der Reihe ist.
     private void setPlayerOnMove() {
 	
+	
+	Player.setonMove = true;
     }
     
+    //Erster Spieler der am Zug ist muss Trumpf definieren.
     private void setTrumpf() {
 	
     }
+    
+    //Spieler spielt eine Karte
+    public void playCard(Card) {
+	
+	countMove();
+	setPlayerOnMove();
+	
+    }
+    //Zählt die Züge einer Runde
+    private void countMove() {
+	if (this.move<4){
+	    this.move++;
+	}
+	else {
+	    this.move = 0;
+	}
+    }
    
     	
-    
+  
     public String toString() {
     	return this.gameID+"";
     }
+    
+    //Getter
     public int getGameID() {
     	return this.gameID;
     }
