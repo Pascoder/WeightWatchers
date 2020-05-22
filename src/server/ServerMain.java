12b36277f2ab0a48faab2dc78edaa57a5deca2ea
Pascal iMac <pascal.wyser@students.fhnw.ch>
2020-05-22 09:47:47 +0200

    aufräumen und anschreiben

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,51 +1,43 @@
 package server;
 
 import java.io.IOException;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+//Pascal Wyser
 
 public class ServerMain {
 	private static int client_id = 0;
 	private final static int port = 9998;
 	private static ServerModel model;
-	
-	//Diese Klasse wartet bis sich ein Client verbindet um dann einen Socket abzuspalten und diesen 
-	//dem ClientThread mitzugeben
-//	
 
-	public static void main(String[] args)throws IOException {
+	/*
+	 * wartet bis sich ein Client verbindet um dann einen Socket abzuspalten und
+	 * diesen dem ClientThread mitzugeben
+	 * 
+	 */
+
+	public static void main(String[] args) throws IOException {
 		ServiceLocator sl = ServiceLocator.getServiceLocator();
 		model = new ServerModel();
 		System.out.println("Server started and listening on port 9998");
-//		sl.getLogger().info("Server started and listening on port "+port);
-		
-		
-		
-		try (ServerSocket serverSocket = new ServerSocket (port, 10, null)){
-			
+
+		try (ServerSocket serverSocket = new ServerSocket(port, 10, null)) {
+
 			while (true) {
 				Socket socket = serverSocket.accept();
 				client_id++;
 
-				
-				ClientThread ct = new ClientThread (socket);
+				ClientThread ct = new ClientThread(socket);
 				ct.start();
-				
-				} 
-				
-			} catch (Exception e) {
-				System.err.println(e.getMessage());
+
 			}
 
-		
-}
-	
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 
-			
-//	   
-	
+	}
 
 }
