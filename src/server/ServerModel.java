ef063f8e46d2ac1713d636cae50ed954111cc56d
Pascal iMac <pascal.wyser@students.fhnw.ch>
2020-05-22 12:21:00 +0200

    ...

--- src/server/ServerModel.java
+++ src/server/ServerModel.java
@@ -75,7 +75,6 @@ public class ServerModel {
 		s = in.readLine();
 	    }
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	accountsloaded = true;
@@ -133,7 +132,7 @@ public class ServerModel {
 	    break;
 
 	case 2:
-	    // TODO In Methoden aufsplitten
+	    // In Methoden aufsplitten
 	    Message_GAMEUPDATE msgOutGame = new Message_GAMEUPDATE();
 
 	    String gameId = Lobby.getLobby().getGameIDofPlayersGame(client);
