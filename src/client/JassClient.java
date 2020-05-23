8ebd78429d0a1380a112364563cb3e90dd1000e3
FrankMauchle <55541627+FrankMauchle@users.noreply.github.com>
2020-05-23 09:16:23 +0200

    fds

--- src/client/JassClient.java
+++ src/client/JassClient.java
@@ -12,73 +12,73 @@ import javafx.stage.Stage;
 //Klasse von Frank Mauchle
 
 public class JassClient extends Application {
-    private static JassClient mainProgram; // singleton
-    private ClientModel clientModel;
-    private ClientView clientView;
-    private ClientController clientController;
-    private static ServiceLocator_JC serviceLocator;
+	private static JassClient mainProgram; // singleton
+	private ClientModel clientModel;
+	private ClientView clientView;
+	private ClientController clientController;
+	private static ServiceLocator_JC serviceLocator;
 
-   
-
-    public static void main(String[] args) {
-	launch(args);
-    }
-
-    @Override
-    //MainProgram als Singleton festlegen
-    public void init() {
-	if (mainProgram == null) {
-	    mainProgram = this;
-	} else {
-	    Platform.exit();
+	public static void main(String[] args) {
+		launch(args);
 	}
-    }
 
-    @Override
-    public void start(Stage stage) {
-	// Initlialize
-	initialize();
-	this.clientModel = new ClientModel();
-	this.clientView = new ClientView(stage, clientModel);
-	this.clientController = new ClientController(clientModel, clientView);
-
-	clientView.start();
-    }
-    //SL, Translator erstellen durch Configuration (JassClient.cfg) Datei
-    public static void initialize() {
-	serviceLocator = ServiceLocator_JC.getServiceLocator();
-	serviceLocator.setLogger(configureLogger());
-	serviceLocator.setConfiguration(new Configuration_JC());
-	String language = serviceLocator.getConfiguration().getOption("Language");
-	serviceLocator.setTranslator(new Translator_JC(language));
-
-    }
-    //Logger konfigurieren
-    private static Logger configureLogger() {
-	Logger rootLogger = Logger.getLogger("");
-	rootLogger.setLevel(Level.FINEST);
-	
-	Handler[] defaultHandlers = Logger.getLogger("").getHandlers();
-	defaultHandlers[0].setLevel(Level.INFO);
-
-	Logger ourLogger = Logger.getLogger(serviceLocator.getAPP_NAME());
-	ourLogger.setLevel(Level.FINEST);
-
-	return ourLogger;
-    }
-
-    //Beim Aufruffen von dieser Methode wird Konfiguration gespeichert
-    public void stop() {
-	serviceLocator.getConfiguration().save();
-	if (clientView != null) {
-	    clientView.stop();
+	@Override
+	// MainProgram als Singleton festlegen
+	public void init() {
+		if (mainProgram == null) {
+			mainProgram = this;
+		} else {
+			Platform.exit();
+		}
 	}
-	serviceLocator.getLogger().info("Application terminated");
-    }
 
-    // Getter für den JassClient
-    protected static JassClient getMainProgram() {
-	return mainProgram;
-    }
+	@Override
+	public void start(Stage stage) {
+		// Initlialize
+		initialize();
+		this.clientModel = new ClientModel();
+		this.clientView = new ClientView(stage, clientModel);
+		this.clientController = new ClientController(clientModel, clientView);
+
+		clientView.start();
+	}
+
+	// SL, Translator erstellen durch Configuration (JassClient.cfg) Datei
+	public static void initialize() {
+		serviceLocator = ServiceLocator_JC.getServiceLocator();
+		serviceLocator.setLogger(configureLogger());
+		serviceLocator.setConfiguration(new Configuration_JC());
+		String language = serviceLocator.getConfiguration().getOption("Language");
+		serviceLocator.setTranslator(new Translator_JC(language));
+
+	}
+
+	// Logger konfigurieren
+	private static Logger configureLogger() {
+		Logger rootLogger = Logger.getLogger("");
+		rootLogger.setLevel(Level.FINEST);
+
+		Handler[] defaultHandlers = Logger.getLogger("").getHandlers();
+		defaultHandlers[0].setLevel(Level.INFO);
+
+		Logger ourLogger = Logger.getLogger(serviceLocator.getAPP_NAME());
+		ourLogger.setLevel(Level.FINEST);
+
+		return ourLogger;
+	}
+
+	// Beim Aufruffen von dieser Methode wird Konfiguration gespeichert
+	public void stop() {
+		serviceLocator.getConfiguration().save();
+		if (clientView != null) {
+			clientView.stop();
+		}
+		serviceLocator.getLogger().info("Application terminated");
+	}
+
+	// Getter für den JassClient
+	protected static JassClient getMainProgram() {
+		return mainProgram;
+	}
 
 }
